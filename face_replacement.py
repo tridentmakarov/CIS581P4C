b24739b18b20b4b970704fc30ea5ef385b4324c2
Sandeep Dcunha <sdcunha@umass.edu>
2017-12-14 15:37:06 -0500

    Added face detection.

--- face_replacement.py
+++ face_replacement.py
@@ -1,2 +1,5 @@
+import cv2
 def detect_faces(img):
-    return None
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return face_cascade.detectMultiScale(gray, 1.3, 5)

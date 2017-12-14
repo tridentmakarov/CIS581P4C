14a9367218e91d3e449533d18062a79a59658d59
Sandeep Dcunha <sdcunha@umass.edu>
2017-12-14 17:41:07 -0500

    Overlay source image over target.

--- face_replacement.py
+++ face_replacement.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.transform import resize
 
 def detect_faces(img):
     face_cascade = cv2.CascadeClassifier('resources/haarcascade_frontalface_default.xml')
@@ -8,18 +9,27 @@ def detect_faces(img):
     return face_cascade.detectMultiScale(gray, 1.3, 5)
 
 def face_replacement(source_vid, target_vid):
-    forground = source_vid.get_data(0)
-    background = target_vid.get_data(0)
+    source = source_vid.get_data(0)
+    target = target_vid.get_data(0)
 
-    faces = detect_faces(background)
-    if faces.size == 0:
+    source_faces = detect_faces(source)
+    target_faces = detect_faces(target)
+    if source_faces.size == 0 or target_faces.size == 0:
         raise ValueError("Face could not be detected in source image")
 
-    replacement_image = background.copy()
-    mask = np.zeros(replacement_image.shape)
-    for (x,y,w,h) in faces:
-        replacement_image[y:y+h, x:x+w] = 0
-        mask[y:y+h, x:x+w] = 1
+    replacement_image = target.copy()
+
+
+    # mask = np.zeros(replacement_image.shape)
+    # for (x,y,w,h) in target_faces:
+    #     replacement_image[y:y+h, x:x+w] = 0
+    #     mask[y:y+h, x:x+w] = 1
+
+    replacement_faces_ims = [source[y:y+h, x:x+w] for (x,y,w,h) in source_faces]
+    replacement_faces_ims = [resize(face, (h, w)) for face, (x,y, w,h)
+                         in zip(replacement_faces_ims, target_faces)]
+    for (x,y,w,h), face in zip(target_faces, replacement_faces_ims):
+        replacement_image[y:y+h, x:x+w, :] = (face * 255).astype(np.uint8)
 
     plt.imshow(replacement_image)
     plt.show()

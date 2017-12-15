e8d9929218b7330027630a3118d664a713c2aca5
Sandeep Dcunha <sdcunha@umass.edu>
2017-12-15 15:51:52 -0500

    Wrote poisson_gray.

--- modified_poisson_blending.py
+++ modified_poisson_blending.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
-import scipy.misc as sc
+import scipy.misc
 import scipy.sparse as sparse
 import matplotlib.pyplot as plt
 from skimage.transform import resize
 
 def modified_poisson_blending(source, target, mask, originalTarget, x_corner, y_corner):
-    source /= 255
-    target /= 255
-    mask /= 255
+    source /= 255.0
+    target /= 255.0
+    mask /= 255.0
 
     F = np.zeros(source.shape)
 
@@ -20,14 +20,23 @@ def modified_poisson_blending(source, target, mask, originalTarget, x_corner, y_
 # Poisson blending using http://vacation.aid.design.kyushu-u.ac.jp/and/poisson/
 def poisson_gray(source, target, mask):
     n = source.size
-    A = sparse(n, n)
     f = np.zeros(n, 1)
-    fx = np.where(mask > 0)
-    bx = np.where(mask == 0)
+    fx = mask > 0
+    bx = mask == 0
     q = np.zeros(n, 1)
     q[fx] = 1
 
-    I = np.diag(sparse(q))
+    I = np.diag(sparse.csc_matrix(q))
     A = -4 * I
+    A += np.roll(I, (0, source.shape[0]), (0,1)) + np.roll(I, (0, - source.shape[0]), (0,1)) +\
+         np.roll(I, (0, 1), (0,1)) + np.roll(I, (0, -1), (0,1))
+    A += sparse.eye(n) - I
+    b = np.zeros(n, 1)
+    b[bx] = target[bx]
 
-    return F
+    laplacian_target = np.roll(target, (1, 0), (0,1)) + np.roll(target, (1, 0), (0,-1)) +\
+                       np.roll(target, (0, 1), (0,1)) + np.roll(target, (0, 1), (0,-1))
+    laplacian_target -= 4*(target.astype(np.int))
+    b[fx] = laplacian_target[fx]
+    x = np.linalg.solve(A, b)
+    return x.reshape(source.shape)

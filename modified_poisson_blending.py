1caba4a70dd7b3f073e297eb204d0a24c8c55455
Sandeep Dcunha <sdcunha@umass.edu>
2017-12-16 21:34:20 -0500

    Shows blend.

--- modified_poisson_blending.py
+++ modified_poisson_blending.py
@@ -21,7 +21,8 @@ def modified_poisson_blending(source_face, target, mask, originalTarget, x_corne
 
     for i in range(3):
         F[:,:,i] = poisson_gray(target[:,:,i], F[:,:,i], mask)
-
+    plt.imshow(F)
+    plt.show()
     modified_img = originalTarget.copy()
     modified_img[x_corner:x_corner + source_face.shape[0],
                 y_corner:y_corner + source_face.shape[1],:] = (F * 255).astype(np.uint8)

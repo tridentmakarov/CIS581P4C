9f5ba30d1f4507a7ad9a453a156c6ded2f802337
tridentmakarov <l.jungmann10@yahoo.com>
2017-12-20 10:40:05 -0500

    Bug, need to debug

--- Main.py
+++ Main.py
@@ -15,11 +15,11 @@ import imageio
 import face_replacement
 
 videolist = ["resources/Medium/LucianoRosso1.mp4", "resources/Medium/LucianoRosso2.mp4", "resources/Medium/LucianoRosso3.mp4",
-             "resources/Hard/Joker.mp4", "resources/Hard/LeonardoDiCaprio.mp4"]
+             "resources/Hard/Joker.mp4", "resources/Hard/LeonardoDiCaprio.mp4", "resources/Trump.mp4"]
 
 
-video1 = imageio.get_reader(videolist[2])
-video2 = imageio.get_reader(videolist[1])
+video1 = imageio.get_reader("resources/Hard/LeonardoDiCaprio.mp4")
+video2 = imageio.get_reader("resources/Easy/MrRobot.mp4")
 f_name = "resources/OutputVideoRedo%d_%d.mp4" % (2, 1)
 face_replacement.face_replacement(video1, video2, f_name, None, debug=False)
 
@@ -40,10 +40,10 @@ def run_face_replacement_no_exceptions(video1, video2, out_filename):
 
 
 for i in range(len(videolist)):
-    for j in range(len(videolist)):
-        if i != j:
-            print "Video %d with video %d" % (i, j)
+    # for j in range(len(videolist)):
+        if i != 5:
+            print "Video %d with video %d" % (i, 5)
             video1 = imageio.get_reader(videolist[i])
-            video2 = imageio.get_reader(videolist[j])
-            f_name = "resources/OutputVideoRedo%d_%d.mp4" % (i, j)
+            video2 = imageio.get_reader(videolist[5])
+            f_name = "resources/OutputVideoRedo%d_%d.mp4" % (i, 5)
             run_face_replacement_no_exceptions(video1, video2, f_name)
